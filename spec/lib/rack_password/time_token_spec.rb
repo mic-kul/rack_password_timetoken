833974e1f94e13397d40a62c417fbfe1b35cc455
Michał Kulesza <michal.kulesza@netguru.co>
2015-10-11 00:49:28 +0200

    More specs

--- spec/lib/rack_password/time_token_spec.rb
+++ spec/lib/rack_password/time_token_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 module RackPassword
   describe TimeToken do
-    let(:time_token) { TimeToken.new(code: 'timecode') }
+    let(:time_token) { TimeToken.new(%w(secret1 secret2)) }
     let(:valid_until) { (Time.now + 60).to_s }
     let(:token) { time_token.generate(valid_until) }
 
@@ -29,6 +29,20 @@ module RackPassword
       let(:expired_token) { time_token.generate(old_time) }
       it 'returns false' do
         expect(time_token.valid?(expired_token, old_time)).to eq false
+      end
+    end
+
+    describe 'for spoofed time until' do
+      let(:time_now) { Time.now }
+      let(:valid_until) { Time.now + 10 }
+      let(:spoofed_valid_until) { valid_until + 60 * 60 }
+      let(:token) { time_token.generate(valid_until) }
+      it 'returns true for correct time' do
+        expect(time_token.valid?(token, valid_until.to_s)).to eq true
+      end
+
+      it 'returns false for spoofed time' do
+        expect(time_token.valid?(token, spoofed_valid_until.to_s)).to eq false
       end
     end
   end
