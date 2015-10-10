8208206ca2cd73ec43f2c16be53cffccf4d1b0dc
Michał Kulesza <michal.kulesza@netguru.co>
2015-10-11 00:56:24 +0200

    Fix token verification
    constructor should contain secret key, signature is obtained using
    update(message) method

--- lib/rack_password.rb
+++ lib/rack_password.rb
@@ -1,6 +1,6 @@
 require 'rack_password/version'
-require 'time'
-require 'hmac-md5'
+require 'hmac-sha2'
+require 'cgi'
 
 module RackPassword
   class Block
@@ -19,13 +19,14 @@ module RackPassword
       return @app.call(env) if bv.valid?
 
       params = request.params
-
       if request.post? and bv.valid_code?(params[@options[:code_param].to_s]) # If post method check :code_param value
         domain = @options[:cookie_domain]
         domain ||= request.host == 'localhost' ? '' : ".#{request.host}"
       [301, {'Location' => request.path, 'Set-Cookie' => "#{@options[:key]}=#{request.params[@options[:code_param].to_s]}; domain=#{domain}; expires=30-Dec-2039 23:59:59 GMT"}, ['']] # Redirect if code is valid
-      elsif request.get? && valid_token?(params[:token], params[:valid_until])
-        [301, {'Location' => request.path, 'Set-Cookie' => "#{@options[:key]}_token=#{params[:token]}; domain=#{domain}; expires=#{valid_until}"}, 'Set-Cookie' => "#{@options[:key]}_time=#{params[:valid_until]}; domain=#{domain}; expires=#{valid_until}"}, ['']] # Redirect if token & time_until are valid
+      elsif request.get? && valid_token?(params['token'], params['valid_until'])
+        [301, {'Location' => request.path, 'Set-Cookie' => "#{@options[:key]}_token=#{params['token']}@#{@options[:key]}_time#{CGI.escape(params['valid_until'])}; domain=#{domain}; expires=#{params['valid_until']}"}, ['']] # Redirect if token & time_until are valid
+      elsif request.get? && bv.valid_code?(params['code'].to_s)
+        success_token_response
       else
         success_rack_response
       end
@@ -35,14 +36,21 @@ module RackPassword
       [200, {'Content-Type' => 'text/html'}, [read_success_view]]
     end
 
+    def success_token_response
+      valid_until = (Time.now.utc + 5 * 60).to_s
+      token = time_token.generate(valid_until)
+      [200, {'Content-Type' => 'text/html'}, ["?token=#{token}&valid_until=#{CGI.escape(valid_until)}"]]
+    end
+
     private
 
     def valid_token?(token, valid_until)
+      return false if token.nil? || valid_until.nil?
       token && valid_until && time_token.valid?(token, valid_until)
     end
 
     def time_token
-      @time_token ||= TimeToken.new(@options[:code_param])
+      @time_token ||= TimeToken.new(@options[:auth_codes])
     end
 
     def read_success_view
@@ -78,26 +86,34 @@ module RackPassword
     end
 
     def valid_token?
-      token = @request.cookies["#{@options[:key].to_s}_token"]
-      valid_until = @request.cookies["#{@options[:key].to_s}_time"]
-      TimeToken.new(@options[:code_param]).valid?(token. valid_until)
+      token_cookie = @request.cookies["#{@options[:key]}_token"]
+      return false if token_cookie.nil?
+      token, valid_until = token_cookie.split("@#{@options[:key]}_time")
+      return false if token.nil? || valid_until.nil?
+      TimeToken.new(@options[:auth_codes]).valid?(token, CGI.unescape(valid_until))
     end
   end
 
   class TimeToken
     attr_accessor :code
 
-    def initialize(code)
-      @code = code
+    def initialize(auth_codes)
+      @code = auth_codes.join(',')
     end
 
     def generate(valid_until)
-      HMAC::MD5.new("code=#{code}&time=#{valid_until}").hexdigest
+      hmac.update("time=#{valid_until}").hexdigest
     end
 
     def valid?(token, time)
-      return false unless token && time && token == generate(time)
-      Time.parse(time) >= Time.now
+      return false if token.nil? || time.nil? || token != generate(time)
+      Time.parse(time) >= Time.now.utc
+    end
+
+    private
+
+    def hmac
+      HMAC::SHA256.new(code)
     end
   end
 end
